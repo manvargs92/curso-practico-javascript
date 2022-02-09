8466f9f8b359796f3f48cbb19c8f3d985ceb653b
manvargs92 <manuel.vargas.garrido@outlook.com>
2022-02-09 12:51:24 -0600

    Escribiendo HTML desde JavaScript

--- descuentos.js
+++ descuentos.js
@@ -1,11 +1,22 @@
 
-const precioOriginal = 120;
-const descuento = 18;
+// const precioOriginal = 120;
+// const descuento = 18;
 
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const inputDiscount = document.getElementById("inputDiscount");
+    const priceValue = inputPrice.value;
+    const discountValue = inputDiscount.value;
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultPrice = document.getElementById("resultPrice");
+    resultPrice.innerText = "El precio con descuento es: $" + precioConDescuento;
 }
 
 
